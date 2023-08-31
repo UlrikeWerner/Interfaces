ed3ce168b36e6dee1d288e3a1425b4f96b3c67d1
UlrikeWerner <ulrike_werner@web.de>
2023-08-31 10:59:18 +0200

    use MediaController in Main

--- src/main/java/com/github/UlrikeWerner/Interfaces/Main.java
+++ src/main/java/com/github/UlrikeWerner/Interfaces/Main.java
@@ -2,6 +2,10 @@ package com.github.UlrikeWerner.Interfaces;
 
 public class Main {
     public static void main(String[] args) {
+        MediaController player = new MediaController();
+        player.playMedia(new MusicPlayer());
+        player.playMedia(new VideoPlayer());
+
 
     }
 }
